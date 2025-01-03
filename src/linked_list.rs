db6b99cfb40df2baedbac734ca9a4370fad07c5f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-03 23:54:21 +0000

    Add linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,7 +1,70 @@
+use std::fmt::{self, Display, Formatter};
+
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList { head: None }
+    }
+
+    pub fn push_head(&mut self, value: T) {
+        let node = Box::new(Node {
+            value,
+            next: self.head.take(),
+        });
+        self.head = Some(node);
+    }
+
+    pub fn pop_head(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.value
+        })
+    }
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Display> Display for LinkedList<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut current = &self.head;
+
+        while let Some(node) = current {
+            write!(f, "{} -> ", node.value)?;
+            current = &node.next;
+        }
+
+        write!(f, "None")
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
-    fn test() {
-        assert_eq!(1 + 1, 2)
+    fn empty() {
+        let list: LinkedList<i32> = LinkedList::new();
+        assert_eq!(list.to_string(), "None");
+    }
+
+    #[test]
+    fn non_empty() {
+        let mut list: LinkedList<i32> = LinkedList::new();
+        list.push_head(1);
+        list.push_head(2);
+        list.push_head(3);
+        assert_eq!(list.to_string(), "3 -> 2 -> 1 -> None");
     }
 }
